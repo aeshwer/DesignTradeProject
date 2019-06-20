3b39b54abb66ddf02a08c1362807ff93cc64adbf
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-06-20 15:20:28 +0530

    RestAPI success

---
+++ restful-java/src/com/restful/java/example/ScoreService.java
@@ -0,0 +1,92 @@
+//Note  : https://www.theserverside.com/video/Step-by-step-RESTful-web-service-example-in-Java-using-Eclipse
+/* Do these on git bash in tomcat folder
+ * 
+ * 1) $ curl -X GET "http://localhost:9090/restful-java/score/wins"
+ * 2)curl -X GET "http://localhost:9090/restful-java/score/wins"
+ * 3)$ curl -X PUT "http://localhost:9090/restful-java/score?wins=11&loss=22&ties=33"
+*/
+package com.restful.java.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class ScoreService {
+
+	private static int wins;
+	private static int loss;
+	private static int ties;
+
+	// {"wins":"5" ,"loss":"1","ties":"0"}
+	@GET
+	@Path("/score")
+	@Produces("application/json")
+	public String getScore() {
+		String pattern = "{\"wins\":\"%s\" ,\"loss\":\"%s\",\"ties\":\"%s\"}";
+		return String.format(pattern, wins, loss, ties);
+
+	}
+
+	// localhost:8080/restful-java/score?wins-2?loss=3@ties=15
+	@PUT
+	@Path("/score")
+	@Produces("application/json")
+	public String updateScore(@QueryParam("wins") int wins, @QueryParam("loss") int loss,
+			@QueryParam("ties") int ties) {
+		ScoreService.wins = wins;
+		ScoreService.loss = loss;
+		ScoreService.ties = ties;
+
+		String pattern = "{\"wins\":\"%s\" ,\"loss\":\"%s\",\"ties\":\"%s\"}";
+		return String.format(pattern, wins, loss, ties);
+
+	}
+
+	@POST
+	@Path("/score/wins")
+	@Produces(MediaType.TEXT_PLAIN)
+	public int increaseWins() {
+		return wins++;
+	}
+
+	@POST
+	@Path("/score/loss")
+	@Produces("text/plain")
+	public int increaseLoss() {
+		return loss++;
+	}
+
+	@POST
+	@Path("/score/ties")
+	@Produces("text/plain")
+	public int increaseTies() {
+		return ties++;
+	}
+
+	@GET
+	@Path("/score/wins")
+	@Produces("text/plain")
+	public int getWins() {
+		return wins;
+	}
+
+	@GET
+	@Path("/score/loss")
+	@Produces("text/plain")
+	public int getLoss() {
+		return loss;
+	}
+
+	@GET
+	@Path("/score/ties")
+	@Produces("text/plain")
+	public int getTies() {
+		return ties;
+	}
+
+}
