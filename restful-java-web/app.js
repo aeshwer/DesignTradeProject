f811f93752f339ace2e34821396137b87d3726e4
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-06-21 10:26:34 +0530

    working fixed CORS

--- restful-java-web/app.js
+++ restful-java-web/app.js
@@ -1,11 +1,28 @@
+// Note how to enable CORS in apache tomcat : https://www.youtube.com/watch?v=2Js3iZXt964
+// add filter mappping in web.xml   ,, link : https://tomcat.apache.org/tomcat-7.0-doc/config/filter.html#CORS_Filter
+
 var myApp = angular.module('myApp', ['ngResource']);
 
-myApp.controller('mainController', ['$scope','$resource', function($scope,$resource) {
+myApp.controller('mainController', ['$scope','$resource','$http', function($scope,$resource,$http) {
     
-    $scope.data = 's';
-    
-    $scope.getData = function() {
-        return $scope.data;
+    $scope.data = 
+        $http.get('http://localhost:9090/restful-java/score/wins')
+        .success(function (result) {
+            $scope.data = result;              
+
+        })
+        .error(function (data, status) {
+
+        });
+        return $scope.data; 
+    $scope.getData = function() {        
+        
     };
+    
+    
+    /*We can also get data this way
+    $scope.weatherAPI = $resource("http://localhost:9090/restful-java/score");    
+    $scope.data = $scope.weatherAPI.get();*/
+          
     
 }]);
